540337a0baaa40a3a0c364b4d2d2c8e084671119
aelsoftware <92710271+aelsoftware@users.noreply.github.com>
2023-07-27 10:32:03 -0700

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -22,16 +22,32 @@ class _MyHomePageState extends State<MyHomePage> {
 
   List<LatLng> points = [];
 
-  String startPointText = "Mark Start Point";
-  String endPointText = "Mark End Point";
-
   bool started = false;
 
   double calculatedArea = 0.00;
 
+  addLocation() async {
+    position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    LocationSettings locationSettings = const LocationSettings(
+      accuracy: LocationAccuracy.high,
+      distanceFilter: 1,
+    );
+
+    Geolocator.getPositionStream(locationSettings: locationSettings)
+        .listen((Position position) {});
+    points.add(LatLng(position.latitude, position.longitude));
+  }
+
+  @override
+  void initState() {
+    points.clear();
+    super.initState();
+  }
+
   @override
   checkGps() async {
-    points.clear();
     servicestatus = await Geolocator.isLocationServiceEnabled();
     if (servicestatus) {
       permission = await Geolocator.checkPermission();
@@ -50,35 +66,12 @@ class _MyHomePageState extends State<MyHomePage> {
       }
 
       if (haspermission) {
+        addLocation();
         setState(() {});
-
-        getLocation();
       }
     } else {
       print("GPS Service is not enabled, turn on GPS location");
     }
-
-    setState(() {});
-  }
-
-  getLocation() async {
-    position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
-
-    LocationSettings locationSettings = const LocationSettings(
-      accuracy: LocationAccuracy.high,
-      distanceFilter: 1,
-    );
-
-    Geolocator.getPositionStream(locationSettings: locationSettings)
-        .listen((Position position) {});
-    Timer.periodic(Duration(seconds: 5), (timer) {
-      points.add(LatLng(position.latitude, position.longitude));
-      if (points.length > 3) {
-        calculatedArea = calculateArea(points);
-        setState(() {});
-      }
-    });
   }
 
   @override
@@ -88,36 +81,63 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("Calculate Area Covered"),
       ),
+      floatingActionButton: FloatingActionButton(
+        isExtended: true,
+        child: Container(
+          width: 200,
+          child: Row(
+            children: [
+              Text("Get Area"),
+              Icon(Icons.arrow_forward_ios),
+            ],
+          ),
+        ),
+        onPressed: () {
+          if (points.length > 3) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => AreaPage(
+                  area: calculatedArea,
+                ),
+              ),
+            );
+          } else {
+            AlertDialog(
+              icon: Icon(Icons.error_outline),
+              title: Text("Please add atleast 4 points to continue"),
+            );
+          }
+        },
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            const Text(
+              "Selected Points",
+              style: TextStyle(fontSize: 18),
+            ),
+            if (points.isNotEmpty)
+              for (int x = 0; x < points.length; x++) ...[
+                Row(
+                  children: [
+                    Text(points[x].latitude.toString()),
+                    const Text(" , "),
+                    Text(points[x].longitude.toString()),
+                  ],
+                ),
+              ],
+            SizedBox(
+              height: 25,
+            ),
             ElevatedButton(
               onPressed: () {
-                started = !started;
-                setState(() {});
                 checkGps();
-
-                if (started == false && points.length > 3) {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => AreaPage(
-                        area: calculatedArea,
-                      ),
-                    ),
-                  );
-                } else {
-                  if (points.length < 3) {
-                    const AlertDialog(
-                      icon: Icon(Icons.error_outlined),
-                      title: Text("Please move around, to add more points"),
-                    );
-                  }
-                }
+                setState(() {});
               },
-              child: Text(started == true ? endPointText : startPointText),
+              child: Text("Mark point ${points.length + 1}"),
             ),
           ],
         ),
